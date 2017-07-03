89fc6c333842986e3455b9da44f2a508de935900
kdufla <ggvel14@freeuni.edu.ge>
2017-07-03 22:12:36 +0400

    HW3 done

--- HW3/project 3.2/sign.py
+++ HW3/project 3.2/sign.py
@@ -1,7 +1,7 @@
 from oracle import *
 from helper import *
 
-# stackoverflow
+# start stackoverflow
 def egcd(a, b):
     if a == 0:
         return (b, 0, 1)
@@ -9,13 +9,13 @@ def egcd(a, b):
         g, y, x = egcd(b % a, a)
         return (g, x - (b // a) * y, y)
 
-# stackoverflow
 def modinv(a, m):
     g, x, y = egcd(a, m)
     if g != 1:
         raise Exception('modular inverse does not exist')
     else:
         return x % m
+# end stackoverflow
 
 def main():
 	n = 119077393994976313358209514872004186781083638474007212865571534799455802984783764695504518716476645854434703350542987348935664430222174597252144205891641172082602942313168180100366024600206994820541840725743590501646516068078269875871068596540116450747659687492528762004294694507524718065820838211568885027869
@@ -29,10 +29,11 @@ def main():
 	m = ascii_to_int(msg)
 
 	s0=Sign(1)
-	s1 = Sign(2)
-	s2 = Sign(m/2)
+	s1=Sign(2)
+	s2=Sign(m/2)
 
 	sigma = (s1 * s2 * modinv(s0, n)) % n
+	assert(sigma < 0)
 
 	if Verify(m, sigma):
 		print "found"
